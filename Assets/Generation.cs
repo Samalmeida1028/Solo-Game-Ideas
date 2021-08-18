9e8f84026d161866475d1cd2b7974f757cab79c7
Samalmeida1028 <80793329+Samalmeida1028@users.noreply.github.com>
2021-08-18 12:28:31 -0400

    start

--- Assets/Generation.cs
+++ Assets/Generation.cs
@@ -4,13 +4,61 @@ using UnityEngine;
 
 public class Generation : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int
+
+            width,
+            height;
+
+    public string seed;
+
+    public bool useRandomSeed;
+
+    [Range(0, 100)]
+    public int randomFillPercent;
+
+    int[,] map;
+
     void Start()
     {
+        GenerateMap();
     }
 
-    // Update is called once per frame
-    void Update()
+    void GenerateMap()
     {
+        map = new int[width, height];
+        RandomFillMap();
+    }
+
+    void RandomFillMap()
+    {
+        if (useRandomSeed)
+        {
+            seed = Time.time.ToString();
+        }
+        System.Random prng = new System.Random(seed.GetHashCode());
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                map[x, y] = (prng.Next(0, 100) < randomFillPercent) ? 1 : 0;
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        if (map != null)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    Gizmos.color = (map[x,y]==1)? Color.black:Color.white;
+                    Vector3 pos = new Vector3(-width/2 + x + .5f,-height/2 + y + .5f,0);
+                    Gizmos.DrawCube(pos,Vector3.one);
+                }
+            }
+        }
     }
 }
